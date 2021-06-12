f1df0a4ecbf1f2ee3e5fcc3f5d3ee8f24fefd2ac
TalhaKhan1530 <68680425+TalhaKhan1530@users.noreply.github.com>
2021-06-12 14:30:50 +0500

    minor change

--- app/src/main/java/com/example/edvisor/Customer_login.java
+++ app/src/main/java/com/example/edvisor/Customer_login.java
@@ -2,9 +2,16 @@ package com.example.edvisor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 public class Customer_login extends AppCompatActivity {
 
@@ -18,7 +25,6 @@ public class Customer_login extends AppCompatActivity {
         customer = (Customer) intent.getSerializableExtra("customer");
         worker = (Edvisor) intent.getSerializableExtra("worker");
     }
-
 
     public void Book_Appointment(View v) {
 
