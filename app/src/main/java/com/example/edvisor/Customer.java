c5733ce2a293e4fa5084e740d36b551e1d157f82
noor <l181236@lhr.nu.edu.pk>
2021-06-19 11:43:48 +0500

    minor

--- app/src/main/java/com/example/edvisor/Customer.java
+++ app/src/main/java/com/example/edvisor/Customer.java
@@ -3,7 +3,7 @@ package com.example.edvisor;
 import java.io.Serializable;
 
 public class Customer implements Serializable {
-    public String name;
-    public int id;
-    public String payment_method;
+  public String name;
+  public int id;
+  public String payment_method;
 }
